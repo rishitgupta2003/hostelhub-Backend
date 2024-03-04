c831be1dc08280b281058bc379f45b632d9182d1
rishitgupta2003 <rishitgupta203@gmail.com>
2024-03-04 21:32:37 +0530

    ErrorCorrected

--- src/util/asyncHandler.js
+++ src/util/asyncHandler.js
@@ -2,7 +2,7 @@ const asyncHandler = (requestHandler) => async (req, res, next) => {
     try{
         return await requestHandler(req, res, next);
     }catch(err){
-        res.status(err.code || 500).json({
+        res.status(err.status || 500).json({
             success: false,
             message: err.message
         });
