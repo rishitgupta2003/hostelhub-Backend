264ab2b4f44ace3d8cae4b2cf632bf21f6eac1e1
rishitgupta2003 <rishitgupta203@gmail.com>
2024-03-04 23:07:58 +0530

    ApiResponse.js

---
+++ src/util/ApiResponse.js
@@ -0,0 +1,14 @@
+class ApiResponse{
+    constructor(
+        statusCode,
+        data,
+        message = "Success"
+    )
+    {
+        this.data = data;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+export { ApiResponse };
