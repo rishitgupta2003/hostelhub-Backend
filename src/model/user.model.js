760e2caf53908090bc8a44fc59e04abdae3b0563
rishitgupta2003 <rishitgupta203@gmail.com>
2024-03-05 23:29:25 +0530

    UserModel

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -2,6 +2,42 @@ import {Schema, model} from "mongoose";
 
 const userSchema = new Schema(
     {
-        
-    }
+        name : {
+            type: String,
+            required: true
+            
+        },
+        uid : {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        gender: {
+            type: String,
+            required: true,
+            enum: ["MALE","FEMALE","OTHER","RATHER NOT SAY"],
+            default: "MALE"
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        avatar: {
+            type: String, //linkOfAvatar
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {timestamps: true}
 )
+
+const User = model("User",userSchema);
