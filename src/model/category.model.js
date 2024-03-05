9a8ef0f639d79597a44d2f794ff31f79a0ddbba7
rishitgupta2003 <rishitgupta203@gmail.com>
2024-03-06 00:49:03 +0530

    CategorySchema

---
+++ src/model/category.model.js
@@ -0,0 +1,17 @@
+import {Schema, model} from "mongoose";
+
+const categorySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Category = model("Category", categorySchema);
