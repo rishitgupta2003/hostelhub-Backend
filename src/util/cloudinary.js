04199121ddaf00f932e30534b8a7b1514c031031
rishitgupta2003 <rishitgupta203@gmail.com>
2024-03-06 19:29:56 +0530

    Configuration

--- src/util/cloudinary.js
+++ src/util/cloudinary.js
@@ -1,11 +1,18 @@
 import { v2 as cloudinary } from "cloudinary";
 
-const uploadOnCloudinary = async () => {
-    
-}
-
 cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        }); //upload file on cloudinary
+    } catch (error) {
+        
+    }
+}
