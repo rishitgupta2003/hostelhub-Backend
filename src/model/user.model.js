ce60a4c5f3981842334f271ca0a3732d548f32da
rishitgupta2003 <rishitgupta203@gmail.com>
2024-03-06 12:38:59 +0530

    AccessToken

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -73,4 +73,20 @@ userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password);
 }
 
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+            _id:this._id,
+            name:this.name,
+            email:this.email,
+            uid:this.uid
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){}
+
 export const User = model("User",userSchema);
