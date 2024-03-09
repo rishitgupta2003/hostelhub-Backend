11c47a29bce664b6c2254789f7eab57f17bdecdb
rishitgupta2003 <rishitgupta203@gmail.com>
2024-03-09 11:48:58 +0530

    Zod Added

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,9 +2,7 @@ import { asyncHandler } from "../util/asyncHandler.js";
 
 const registerUser = asyncHandler(
     async (req, res) => {
-        res.status(200).json({
-            msg : "ok"
-        });
+        
     }
 )
 
