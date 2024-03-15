9f746c9e2b3347ca46bf42177f13c3dcd8ea19da
rishitgupta2003 <rishitgupta203@gmail.com>
2024-03-15 13:27:43 +0530

    Update User Routes Resolved

--- src/routes/update.routes.js
+++ src/routes/update.routes.js
@@ -1,6 +1,6 @@
 import { Router } from  "express";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-import { updatePassword, updateHostel, updateAvatar } from "../controllers/update.controllers.js";
+import { updatePassword, updateHostel, updateAvatar } from "../controllers/updateUser.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router();
