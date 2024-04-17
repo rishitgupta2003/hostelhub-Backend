58b8f733da80a6481a05130f894023ee8815ccc3
rishitgupta2003 <rishitgupta203@gmail.com>
2024-04-17 13:30:21 +0530

    Size Increased

--- src/app.js
+++ src/app.js
@@ -14,12 +14,12 @@ app.use(cors({
 }));
 
 app.use(express.json({
-    limit: "16kb"
+    limit: "50mb"
 }))
 
 app.use(express.urlencoded({
     extended: true,
-    limit: "16kb"
+    limit: "50mb"
 }))
 
 app.use(express.static("/public"));
