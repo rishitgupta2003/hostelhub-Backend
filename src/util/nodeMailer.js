2e8e3f0befc059c1b2489710c8d809f36607e583
rishitgupta2003 <rishitgupta203@gmail.com>
2024-04-18 17:20:09 +0530

    Updated NodeMailer

--- src/util/nodeMailer.js
+++ src/util/nodeMailer.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secureConnection: true,
+    secure: true,
     auth: {
         user: process.env.MAIL,
         pass: process.env.MAIL_PASS
